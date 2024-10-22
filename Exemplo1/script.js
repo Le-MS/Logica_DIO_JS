36d279f86674998f8a9210bebbfbe2e84a06c5e4
Leticia <l.souza.lms@gmail.com>
2024-10-22 17:15:13 -0300

    primeira atualização

---
+++ Exemplo1/script.js
@@ -0,0 +1,17 @@
+console.log("Digite o nome do seu jogador: ")
+
+let nome = "Leticia"
+
+console.log("Bem vinda " + nome)
+
+//Vetores
+let frutas = ["maca", "pera", "banana"]
+console.log(frutas[0])
+frutas.pop()//Remove o ultimo item do vetor
+frutas.shift()//Remove o primeiro item do vetor
+
+//Matriz
+let pessoas = [
+    ["Ana", "advogada",21],
+    ["Joao", "medico",40]
+]
